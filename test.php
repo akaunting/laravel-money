7cc44f50df84cf956e166f70c46034f96c3aecfd
Denis Duliçi <denisdulici@gmail.com>
2022-04-27 23:39:56 +0300

    fixed `test.php`

--- test.php
+++ test.php
@@ -5,6 +5,6 @@ use Akaunting\Money\Money;
 
 require 'vendor/autoload.php';
 
-$money = Money::USD(1000)->convert(Currency::CAD());
+$money = Money::USD(1000)->convert(Currency::CAD(), 1.0);
 
 die($money);
