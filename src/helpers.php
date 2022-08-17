9380010a4b938a5d80469f2caf5e576aeae05084
Guido Belluomo <guidobelluomo@gmail.com>
2022-08-17 21:55:59 +0200

    @currency respects default and default env value for $convert

--- src/helpers.php
+++ src/helpers.php
@@ -4,11 +4,16 @@ use Akaunting\Money\Currency;
 use Akaunting\Money\Money;
 
 if (!function_exists('money')) {
-    function money(mixed $amount, string $currency = null, bool $convert = false): Money
+    function money(mixed $amount, string $currency = null, bool $convert = null): Money
     {
         if (is_null($currency)) {
             /** @var string $currency */
             $currency = env('DEFAULT_CURRENCY', 'USD');
+        }
+        
+        if (is_null($convert)) {
+            /** @var bool $currency */
+            $convert = env('CONVERT_CURRENCY_DEFAULT', false);
         }
 
         return new Money($amount, currency($currency), $convert);
@@ -16,8 +21,13 @@ if (!function_exists('money')) {
 }
 
 if (!function_exists('currency')) {
-    function currency(string $currency): Currency
+    function currency(string $currency = null): Currency
     {
+        if (is_null($currency)) {
+            /** @var string $currency */
+            $currency = env('DEFAULT_CURRENCY', 'USD');
+        }
+        
         return new Currency($currency);
     }
 }
